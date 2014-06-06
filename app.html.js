8f57719317602e291de6a481f7a1ef8ac17e9121
Tiago Stutz <tiago@stutzsolucoes.com.br>
2014-06-06 19:31:42 -0300

    Update app.html.js

--- app.html.js
+++ app.html.js
@@ -306,224 +306,196 @@ function AppViewModel() {
 			stickersInfo.timeElapsedInfo(info);
 		}
 	}, 5000);
-}
 
-AppViewModel.prototype.connectAndPublishSelfInfo = function() {
-	var _self = this;
-	this.connectToMQTTServer(this.mainTopicName, function() {
-		_self.publishStickersInfoToServer();
-	});
-}
 
-AppViewModel.prototype.publishStickersInfoToServer = function() {
-	if(this.mqttConnected) {
-		console.log("Preparing stickers info...");
-		var _self = this;
-		var stickersInfo = {
-			clientUUID: _self.clientUUID,
-			time: new Date().getTime(),
-			nickname: _self.nickname.peek(),
-			place: _self.place.peek(),
-			selfInfo: _self.selfInfo.peek(),
-			neededStickers: AppViewModel.getOnlySelectedItems(_self.neededStickersModel.items),
-			availableStickers: AppViewModel.getOnlySelectedItems(_self.availableStickersModel.items)
-			//stickersForReceivingFromPeer: Array - used later during ranking calculations
-			//stickersForGivingToPeer: Array - used later during ranking calculations
-		}
-		console.log("Publishing stickers info to MQTT server...");
-		this.publishToMQTTServer(this.mainTopicName, JSON.stringify(stickersInfo));
-	} else {
-		console.log("Not connected to server");
+	_self.connectAndPublishSelfInfo = function() {
+		_self.connectToMQTTServer(_self.mainTopicName, function() {
+			_self.publishStickersInfoToServer();
+		});
 	}
-}
-AppViewModel.getOnlySelectedItems = function(items) {
-	var result = new Array();
-	for(var i=0; i<items.length; i++) {
-		if(items[i].selected()) {
-			result.push(items[i]);
+
+	_self.publishStickersInfoToServer = function() {
+		if(_self.mqttConnected) {
+			console.log("Preparing stickers info...");
+			var stickersInfo = {
+				clientUUID: _self.clientUUID,
+				time: new Date().getTime(),
+				nickname: _self.nickname.peek(),
+				place: _self.place.peek(),
+				selfInfo: _self.selfInfo.peek(),
+				neededStickers: _self.getOnlySelectedItems(_self.neededStickersModel.items),
+				availableStickers: _self.getOnlySelectedItems(_self.availableStickersModel.items)
+				//stickersForReceivingFromPeer: Array - used later during ranking calculations
+				//stickersForGivingToPeer: Array - used later during ranking calculations
+			}
+			console.log("Publishing stickers info to MQTT server...");
+			_self.publishToMQTTServer(_self.mainTopicName, JSON.stringify(stickersInfo));
+		} else {
+			console.log("Not connected to server");
 		}
 	}
-	return result;
-}
 
-AppViewModel.getOnlySelectedItems = function(items) {
-	var result = new Array();
-	for(var i=0; i<items.length; i++) {
-		if(items[i].selected()) {
-			result.push(items[i]);
+	_self.getOnlySelectedItems = function(items) {
+		var result = new Array();
+		for(var i=0; i<items.length; i++) {
+			if(items[i].selected()) {
+				result.push(items[i]);
+			}
 		}
+		return result;
 	}
-	return result;
-}
 
-AppViewModel.prototype.disconnectFromMQTTServer = function() {
-	if(this.mqttClient!=null) {
-		console.log("Disconnecting from MQTT server...");
-		try {
-			this.mqttClient.disconnect();
-			this.mqttClient = null;
-			this.mqttConnected = false;
-			this.showResultados();
- 		} catch (e) {
- 			console.log(e);
- 		}
- 	}
-}
+	_self.getOnlySelectedItems = function(items) {
+		var result = new Array();
+		for(var i=0; i<items.length; i++) {
+			if(items[i].selected()) {
+				result.push(items[i]);
+			}
+		}
+		return result;
+	}
 
-AppViewModel.prototype.connectToMQTTServer = function(mainTopicName, onSuccess) {
-	//store info for later use
-	localStorage["nickname"] = this.nickname();
-	localStorage["place"] = this.place();
-	localStorage["selfInfo"] = this.selfInfo();
-
-	//connect to mqtt server
-	var _self = this;
-	this.disconnectFromMQTTServer();
-	this.mqttClient = new Messaging.Client("gostutz.com", 61623, new Date().getTime()+"");
-	var options = {
-		timeout : 10,
-		onSuccess : function() {
+	_self.disconnectFromMQTTServer = function() {
+		if(_self.mqttClient!=null) {
+			console.log("Disconnecting from MQTT server...");
 			try {
-				_self.mqttConnected = true;
-				console.log("Subscribing to '"+mainTopicName+"'...");
-				_self.mqttClient.subscribe(mainTopicName);
-				if(onSuccess) {
-					onSuccess.call();
+				_self.mqttClient.disconnect();
+				_self.mqttClient = null;
+				_self.mqttConnected = false;
+				_self.showResultados();
+	 		} catch (e) {
+	 			console.log(e);
+	 		}
+	 	}
+	}
+
+	_self.connectToMQTTServer = function(mainTopicName, onSuccess) {
+		//store info for later use
+		localStorage["nickname"] = _self.nickname;
+		localStorage["place"] = _self.place;
+		localStorage["selfInfo"] = _self.selfInfo;
+
+		//connect to mqtt server
+		_self.disconnectFromMQTTServer();
+		_self.mqttClient = new Messaging.Client("gostutz.com", 61623, new Date().getTime()+"");
+		var options = {
+			timeout : 10,
+			onSuccess : function() {
+				try {
+					_self.mqttConnected = true;
+					console.log("Subscribing to '"+mainTopicName+"'...");
+					_self.mqttClient.subscribe(mainTopicName);
+					if(onSuccess) {
+						onSuccess.call();
+					}
+					_self.showPessoas();
+				} catch (e) {
+					console.log(e);
 				}
-				_self.showPessoas();
-			} catch (e) {
+			},
+			onFailure : function(responseObject) {
+				_self.mqttConnected = false;
+				console.log("Failure:"+responseObject.errorCode+" "+responseObject.errorMessage);
+			},
+			userName: "user",
+			password: "user",
+			useSSL: false
+		};
+
+		_self.mqttClient.onMessageArrived = function(message) {
+			try {
+				console.log("Message arrived: " + message.payloadString);
+				stickersInfo = JSON.parse(message.payloadString);
+				stickersInfo.timeElapsedInfo = ko.observable("Há poucos segundos");
+				
+				//remove previous results from peer
+				var index = -1;
+				for(var i=0; i<_self.receivedStickersInfo.length; i++) {
+					if(stickersInfo.clientUUID==_self.receivedStickersInfo[i].clientUUID) {
+						index = i;
+					}
+				}
+				
+				if(index > -1) {
+					_self.receivedStickersInfo.splice(index,1);
+				}
+				
+				_self.receivedStickersInfo.push(stickersInfo);
+				_self.recalculateStickersInfoRanking();
+			} catch(e) {
 				console.log(e);
 			}
-		},
-		onFailure : function(responseObject) {
-			_self.mqttConnected = false;
-			console.log("Failure:"+responseObject.errorCode+" "+responseObject.errorMessage);
-		},
-		userName: "user",
-		password: "user",
-		useSSL: false
-	};
-	var _self = this;
-	this.mqttClient.onMessageArrived = function(message) {
-		try {
-			console.log("Message arrived: " + message.payloadString);
-			stickersInfo = JSON.parse(message.payloadString);
-			stickersInfo.timeElapsedInfo = ko.observable("Há poucos segundos");
-			
-			//remove previous results from peer
-			var index = -1;
+		};
+		
+		_self.onConnectionLost = function(message) {
+			console.log("Connection lost:"+message.errorCode + " " +responseObject.errorMessage);
+		};
+
+		console.log("Connecting to MQTT server...");
+		_self.mqttClient.connect(options);
+	}
+
+	_self.publishToMQTTServer = function(topicName, payload) {
+		var message = new Messaging.Message(payload);
+		message.destinationName = topicName;
+		_self.mqttClient.send(message); 
+	}
+
+	_self.recalculateStickersInfoRanking = function() {
+		if(_self.receivedStickersInfo!=null && _self.neededStickersModel!=null && _self.availableStickersModel!=null) {
+
+			//look for stickers that the current user needs and are available from others
 			for(var i=0; i<_self.receivedStickersInfo.length; i++) {
-				if(stickersInfo.clientUUID==_self.receivedStickersInfo[i].clientUUID) {
-					index = i;
-				}
-			}
-			
-			if(index > -1) {
-				_self.receivedStickersInfo.splice(index,1);
-			}
-			
-			_self.receivedStickersInfo.push(stickersInfo);
-			_self.recalculateStickersInfoRanking();
-		} catch(e) {
-			console.log(e);
-		}
-	};
-	this.onConnectionLost = function(message) {
-		console.log("Connection lost:"+message.errorCode + " " +responseObject.errorMessage);
-	};
-
-	console.log("Connecting to MQTT server...");
-	this.mqttClient.connect(options);
-}
-AppViewModel.prototype.publishToMQTTServer = function(topicName, payload) {
-	var message = new Messaging.Message(payload);
-	message.destinationName = topicName;
-	this.mqttClient.send(message); 
-}
-
-//perform balance line matches and ranking seeking for the best people that could exchange stickers with the user
-AppViewModel.prototype.recalculateStickersInfoRanking = function() {
-	if(this.receivedStickersInfo!=null && this.neededStickersModel!=null && this.availableStickersModel!=null) {
-
-		//look for stickers that the current user needs and are available from others
-		for(var i=0; i<this.receivedStickersInfo.length; i++) {
-			var receivedStickerInfo = this.receivedStickersInfo[i];
-			receivedStickerInfo.stickersForReceivingFromPeer = new Array();
-			receivedStickerInfo.stickersForGivingToPeer = new Array();
-	
-			//find stickers that the current user could get from other peers
-			for(var j=0; j<this.neededStickersModel.items.length; j++) {
-				var neededStickerByUser = this.neededStickersModel.items[j];
+				var receivedStickerInfo = _self.receivedStickersInfo[i];
+				receivedStickerInfo.stickersForReceivingFromPeer = new Array();
+				receivedStickerInfo.stickersForGivingToPeer = new Array();
 		
-				for(var k=0; k<receivedStickerInfo.availableStickers.length; k++) {
-					var availableStickerFromPeer = receivedStickerInfo.availableStickers[k];
-					if(neededStickerByUser.number==availableStickerFromPeer.number && neededStickerByUser.selected()) {
-						receivedStickerInfo.stickersForReceivingFromPeer.push({number: availableStickerFromPeer.number, selected:true});
+				//find stickers that the current user could get from other peers
+				for(var j=0; j<_self.neededStickersModel.items.length; j++) {
+					var neededStickerByUser = _self.neededStickersModel.items[j];
+			
+					for(var k=0; k<receivedStickerInfo.availableStickers.length; k++) {
+						var availableStickerFromPeer = receivedStickerInfo.availableStickers[k];
+						if(neededStickerByUser.number==availableStickerFromPeer.number && neededStickerByUser.selected()) {
+							receivedStickerInfo.stickersForReceivingFromPeer.push({number: availableStickerFromPeer.number, selected:true});
+						}
+					}
+				}
+
+				//find stickers that the current user could give to other peers
+				for(var j=0; j<_self.availableStickersModel.items.length; j++) {
+					var availableStickerByUser = _self.availableStickersModel.items[j];
+			
+					for(var k=0; k<receivedStickerInfo.neededStickers.length; k++) {
+						var neededStickerFromPeer = receivedStickerInfo.neededStickers[k];
+						if(availableStickerByUser.number==neededStickerFromPeer.number && availableStickerByUser.selected()) {
+							receivedStickerInfo.stickersForGivingToPeer.push({number: availableStickerByUser.number, selected:true});
+						}
 					}
 				}
 			}
 
-			//find stickers that the current user could give to other peers
-			for(var j=0; j<this.availableStickersModel.items.length; j++) {
-				var availableStickerByUser = this.availableStickersModel.items[j];
-		
-				for(var k=0; k<receivedStickerInfo.neededStickers.length; k++) {
-					var neededStickerFromPeer = receivedStickerInfo.neededStickers[k];
-					if(availableStickerByUser.number==neededStickerFromPeer.number && availableStickerByUser.selected()) {
-						receivedStickerInfo.stickersForGivingToPeer.push({number: availableStickerByUser.number, selected:true});
+			//order ranking by best matches
+			if(_self.receivedStickersInfo.length>0) {
+				_self.receivedStickersInfo.sort(function(left,right) {
+					if(left.stickersForReceivingFromPeer.length > right.stickersForReceivingFromPeer.length) {
+						return 1;
+					} else {
+						return -1;
 					}
+				});
+				
+				//update screen items
+				_self.receivedStickersScreen.removeAll();
+				for(var i=0; i<_self.receivedStickersInfo.length; i++) {
+					_self.receivedStickersScreen.push(_self.receivedStickersInfo[i]);
 				}
 			}
 		}
 
-		//order ranking by best matches
-		if(this.receivedStickersInfo.length>0) {
-			this.receivedStickersInfo.sort(function(left,right) {
-				if(left.stickersForReceivingFromPeer.length > right.stickersForReceivingFromPeer.length) {
-					return 1;
-				} else {
-					return -1;
-				}
-			});
-			
-			//update screen items
-			this.receivedStickersScreen.removeAll();
-			for(var i=0; i<this.receivedStickersInfo.length; i++) {
-				this.receivedStickersScreen.push(this.receivedStickersInfo[i]);
-			}
-		}
 	}
 
 }
-
-AppViewModel.prototype.showSection = function(elementId) {
-	$("article").hide(); //hide all articles (app pages)
-	$("#"+elementId).show();
-}
-AppViewModel.prototype.showFaltandoSection = function() {
-	this.currentSection(this.interactionNeededStickers);
-}
-AppViewModel.prototype.showRepetidaNeededStickers = function() {
-	this.currentSection(this.interactionAvailableStickers);
-}
-AppViewModel.prototype.showTrocarAgoraSection = function() {
-	if (this.mqttConnection == null) {
-		this.currentSection(this.conexaoSection);
-	} else {
-		this.currentSection(this.listaMatchesSection);
-	}
-}
-AppViewModel.prototype.showPessoas = function() {
-	showSection("lista-matches");
-	document.getElementById("pessoas").style.display = "none";
-	document.getElementById("arena-troca").style.display = "block";
-}
-AppViewModel.prototype.showArenaTroca = function() {
-	showSection("lista-matches");
-	document.getElementById("pessoas").style.display = "none";
-	document.getElementById("arena-troca").style.display = "block";
-}
-
 
 //***** /end of VIEW VIEWMODELS ******//
 
